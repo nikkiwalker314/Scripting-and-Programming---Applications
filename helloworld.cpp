64f36c50ca25ff3cbc30a7cb7be68af5fabe597e
nikkiwalker <nwal314@wgu.edu>
2025-08-09 15:31:29 -0500

    removed Hellow World and added Salaray

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int wage;
+
+    wage = 20;
+
+    cout << "Salary is ";
+    cout << wage * 40 * 52;
+    cout << endl;
+
     return 0;
 }
